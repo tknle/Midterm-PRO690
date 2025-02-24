2b6c273da511098d0091ac6a8be188fcffa14cbf
John Doe <johndoe@example.com>
2025-02-23 23:38:38 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PRO690Midterm.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
